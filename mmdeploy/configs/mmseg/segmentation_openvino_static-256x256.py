c06041b1eaaa6239236d224266a730d3239bd393
YoungHowYou <YoungHowYou@outlook.com>
2025-12-10 20:44:38 +0800

    MMDeploy完备

---
+++ mmdeploy/configs/mmseg/segmentation_openvino_static-256x256.py
@@ -0,0 +1,4 @@
+_base_ = ['./segmentation_static.py', '../_base_/backends/openvino.py']
+onnx_config = dict(input_shape=[256, 256])
+backend_config = dict(
+    model_inputs=[dict(opt_shapes=dict(input=[1, 3, 256, 256]))])
